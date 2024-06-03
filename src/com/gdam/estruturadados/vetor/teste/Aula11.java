4dee2bc46b1adc402bff5f509231772e05ba6d17
gustavomarchesim <gustavomarchesim@outlook.com>
2024-06-03 17:23:26 -0300

    Implementanto tipos genéricos para as Listas

---
+++ src/com/gdam/estruturadados/vetor/teste/Aula11.java
@@ -0,0 +1,14 @@
+package com.gdam.estruturadados.vetor.teste;
+
+import com.gdam.estruturadados.vetor.Lista;
+
+public class Aula11 {
+    public static void main(String[] args) {
+
+        @SuppressWarnings({ "unchecked", "rawtypes" })
+        Lista<String> vetor = new Lista(1);
+
+        vetor.adiciona("Elemento");
+    }
+
+}
