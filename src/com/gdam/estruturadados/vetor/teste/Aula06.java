2bbcbdc950e7acc3cb1ffe0281673a050433d788
gustavomarchesim <gustavomarchesim@outlook.com>
2024-06-03 14:42:57 -0300

    Adiciona funcionalidade busca no vetor

---
+++ src/com/gdam/estruturadados/vetor/teste/Aula06.java
@@ -0,0 +1,15 @@
+package com.gdam.estruturadados.vetor.teste;
+
+import com.gdam.estruturadados.vetor.Vetor;
+
+public class Aula06 {
+    public static void main(String[] args) {
+        Vetor vetor = new Vetor(10);
+
+        vetor.adiciona("1");
+        vetor.adiciona("2");
+        vetor.adiciona("3");
+
+        System.out.println(vetor.busca("4"));
+    }
+}
