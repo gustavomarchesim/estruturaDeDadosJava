0501f6df7f100f9cdd322567e4a440c4a0377c6f
gustavomarchesim <gustavomarchesim@outlook.com>
2024-06-18 18:38:12 -0300

    Adiciona referencia ao ultimo elemento

--- src/com/gdam/estruturadados/lista/ListaEncadeada.java
+++ src/com/gdam/estruturadados/lista/ListaEncadeada.java
@@ -3,12 +3,21 @@ package com.gdam.estruturadados.lista;
 public class ListaEncadeada<T> {
 
     private No<T> inicio;
+    private No<T> ultimo;
     private int tamanho;
 
     public void adiciona(T elemento) {
         No<T> celula = new No<T>(elemento);
-        this.inicio = celula; // Aponta o inicio para a célula inicial
+
+        if (this.tamanho == 0) {
+            this.inicio = celula; // Aponta o inicio para a célula inicial
+        } else {
+            this.ultimo.setProximo(celula); // Aponta o elemento adicionado como sendo o ultimo e por consequencia, o
+                                            // proximo.
+        }
+        this.ultimo = celula;
         this.tamanho++;
+
     }
 
     public int getTamanho() {
