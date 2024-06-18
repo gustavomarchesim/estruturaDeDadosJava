a32d24c202c7d2c7a5eb06fe46fd6bd8fc6fc5db
gustavomarchesim <gustavomarchesim@outlook.com>
2024-06-18 17:46:26 -0300

    Retorna o tamanho da lista

--- src/com/gdam/estruturadados/lista/ListaEncadeada.java
+++ src/com/gdam/estruturadados/lista/ListaEncadeada.java
@@ -1,11 +1,18 @@
 package com.gdam.estruturadados.lista;
 
 public class ListaEncadeada<T> {
+
     private No<T> inicio;
+    private int tamanho;
 
     public void adiciona(T elemento) {
         No<T> celula = new No<T>(elemento);
         this.inicio = celula; // Aponta o inicio para a célula inicial
+        this.tamanho++;
+    }
+
+    public int getTamanho() {
+        return this.tamanho;
     }
 
     @Override
